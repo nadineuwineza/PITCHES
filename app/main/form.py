12eff217dd2cee401dc32d4f6f68be8bbad8e005
nadineuwineza <nadineuwineza2017@gmail.com>
2020-12-07 07:36:53 +0200

    added forms

--- app/main/form.py
+++ app/main/form.py
@@ -2,7 +2,6 @@ from flask_wtf import FlaskForm
 from wtforms import StringField,TextAreaField,SubmitField
 from wtforms.validators import Required
 
-
 class ReviewForm(FlaskForm):
 
     title = StringField('Review title',validators=[Required()])
