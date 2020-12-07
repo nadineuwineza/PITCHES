6464ff42f6bd62dc0533f3b24134021c1054f532
nadineuwineza <nadineuwineza2017@gmail.com>
2020-12-07 07:11:09 +0200

    added registration

---
+++ app/config.py
@@ -0,0 +1,38 @@
+import os
+
+class Config:
+
+
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://nadine:uwineza123@localhost/pitches'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+
+    # email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+
+    # simple mde  configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+
+class ProdConfig(Config):
+    pass
+
+
+
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://nadine:uwineza123@localhost/pitches_test'
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://nadine:uwineza123@localhost/pitches'
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+'test':TestConfig
+}
+    
---
+++ start.sh
@@ -0,0 +1,4 @@
+export MAIL_USERNAME='nadineuwineza2017@gmail.com'
+export MAIL_PASSWORD='uwineza123'
+
+python3.8 manage.py server
