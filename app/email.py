385ddc95c6e6ad81624a7d44689669b439d35d8f
nadineuwineza <nadineuwineza2017@gmail.com>
2020-12-07 07:43:05 +0200

    added mail message

--- app/email.py
+++ app/email.py
@@ -2,6 +2,7 @@ from flask_mail import Message
 from flask import render_template
 from . import mail
 
+
 def mail_message(subject,template,to,**kwargs):
     sender_email = 'nadineuwineza2017@gmail.com'
 
